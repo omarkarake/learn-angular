a738acec99665e2a4fe8240fe15a8bed3c9bad9c
omar karake <omar.karake@amalitechtraining.org>
2024-07-02 19:10:39 +0200

    how to pass data

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'course-card',
-  templateUrl: './course-card.component.html',
-  styleUrl: './course-card.component.css'
+  selector: "course-card",
+  templateUrl: "./course-card.component.html",
+  styleUrls: ["./course-card.component.css"],
 })
 export class CourseCardComponent {
-
+  @Input()
+  title: string;
 }
