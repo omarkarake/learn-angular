b0348824c4d3e30eb28a0c187af725dcf5540456
omar karake <omar.karake@amalitechtraining.org>
2024-07-03 08:39:00 +0200

    at empty feature when there is not data found

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,7 @@ import { Course } from "./model/course";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-
-  courses = COURSES;
+  courses = [...COURSES];//if we don't have anything it will display the @empty feature stament
 
   onCourseSelected(course: Course) {
     console.log("card clicked", course);
